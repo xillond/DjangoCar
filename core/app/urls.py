028f730d334305fe55ff4cae86e700a0ae6803fe
xillond <akunbaevalidin@gmail.com>
2025-02-17 22:26:19 +0600

    добавил функцию добавление машины через сайт

--- core/app/urls.py
+++ core/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('details/<int:pk>', views.details_page, name='details')
+    path('details/<int:pk>', views.details_page, name='details'),
+    path('add_car/', views.add_car, name='add_car'),
 ]
--- core/app/views.py
+++ core/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car, Category, Color
 
 def home_page(request):
@@ -9,3 +9,30 @@ def home_page(request):
 def details_page(request, pk):
     car = Car.objects.get(id=pk)
     return render(request, 'app/details.html', {'car': car})
+
+def add_car(request):
+
+    colors = Color.objects.all()
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        category_id = request.POST['category_id']
+        description = request.POST['description']
+        title = request.POST['title']
+        model = request.POST['model']
+        year = request.POST['year']
+        engine_capacity = request.POST['engine_capacity']
+        odometer = request.POST['odometer']
+        color_id = request.POST['color_id']
+        image = request.FILES['image']
+
+        color = Color.objects.get(id=color_id)
+        category = Category.objects.get(id=category_id)
+
+        new_car = Car(title=title, description=description, model=model,year=year,
+                    engine_capacity=engine_capacity, odometer=odometer, color=color, category=category, image=image)
+        new_car.save()
+        return redirect('home')
+
+    return render(request, 'app/add_car.html', {'colors': colors, 'categories': categories})
+
